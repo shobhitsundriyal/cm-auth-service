95cb298be704be1645c546cbb93de6ba70cbd79f
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2025-05-26 18:22:38 +0530

    Remove intentionally created error

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
 import express, { NextFunction, Request, Response } from "express";
 import logger from "./config/logger";
-import createHttpError, { HttpError } from "http-errors";
+import { HttpError } from "http-errors";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
-  const err = createHttpError(401, "Unauthorized access");
-  //throw err; //this fails (not caught by error handler) in case this cb is async (also server will crash out) which in most case it would be. rather use next(err) to pass the error to the global error handler
-
-  next(err);
   res.send("Welcome to auth service");
 });
 
