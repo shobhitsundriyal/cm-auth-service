5aa1f95f6d8d9648f79d0efdcef8b06e261bc869
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2025-05-26 17:56:03 +0530

    setup logger

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = () => {
-  const port = Config.PORT;
+  const PORT = Config.PORT;
 
   try {
-    app.listen(port, () => {
-      console.log(
-        `Server is running on port ${port} in ${Config.NODE_ENV} mode`
+    app.listen(PORT, () => {
+      logger.info(
+        `Server is running on port ${PORT} in ${Config.NODE_ENV} mode`
       );
     });
   } catch (error) {
