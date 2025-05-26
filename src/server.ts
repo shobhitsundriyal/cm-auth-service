35400e1cc693412a02d799ffea6b7be15c55ca23
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2025-05-26 17:36:22 +0530

    Express js setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
+import app from "./app";
 import { Config } from "./config";
 
-function welcomeMessage(name: string) {
-  const user = { name: "shobit" };
+const startServer = () => {
+  const port = Config.PORT;
 
-  console.log("welcome ", user.name);
-}
-
-welcomeMessage("Yoyoy");
-console.log(Config.PORT);
+  try {
+    app.listen(port, () => {
+      console.log(
+        `Server is running on port ${port} in ${Config.NODE_ENV} mode`
+      );
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    process.exit(1);
+  }
+};
+startServer();
